447d5a059cad89c53a8c47e4299c36a217b6db6c
Sefd1Relax <dascalescuadrian1@gmail.com>
2025-01-17 20:51:10 +0200

    Remove the version

--- bot_v4.py
+++ bot_v4.py
@@ -17,8 +17,6 @@ from colorama import Fore, Back, Style
 import socket
 import pyautogui
 import subprocess
-import g4f
-from g4f import ChatCompletion
 
 #---------------------------------------
 # Дизайн
@@ -114,26 +112,6 @@ def callback_handler(call):
 
 
 bot.send_message(config.CHAT_ID, text=message_text, reply_markup=keyboard, parse_mode="Markdown")
-
-
-
-@bot.message_handler(func=lambda message: True)
-def reply_to_user(message):
-    question = message.text
-
-    response = ChatCompletion.create(
-        model="gpt-4",
-        temperature=0.9,
-        max_tokens=1000,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=["You:"],
-        messages=[{"role": "system", "content": ("Ты должен помогать пользователям и отвечать на все вопросы", "Твоё имя - Артем, ты искусственный интелект от Дмитрий Нелова", "Ты должен помогать пользователям")}, {"role": "user", "content": question}]
-    )
-
-    bot.reply_to(message, response)
-
 
 
 @bot.message_handler(commands=["sct", "screenshot"])
