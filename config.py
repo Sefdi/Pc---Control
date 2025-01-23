fdc19eab566a7cab7fee5681368c0d58c30ce0d0
Sefd1Relax <106656773+Sefdi@users.noreply.github.com>
2025-01-23 21:55:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Version V5
 # By Sefd1Relax // github.com/Sefdi
 
-TOKEN = "" # Токен можно получить в боте @FatherBot
+TOKEN = "" # Токен можно получить в боте @BotFather
 CHAT_ID = "" # Айди вашего аккаунта можно получить в боте @userinfobot
